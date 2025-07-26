e6f20331316d2a725ff382316a2bc20c21006101
rahulmuralitechnology <chand0722@yahoo.com>
2025-07-26 22:15:08 +0530

    optimization

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,7 @@
 import type { NextConfig } from "next";
-import withBundleAnalyzer from "@next/bundle-analyzer";
-import withPWA from "next-pwa";
-
-const pwaConfig = withPWA({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
-  register: true,
-  skipWaiting: true,
-}) as (config: NextConfig) => NextConfig; // Type assertion
-
-const bundleAnalyzer = withBundleAnalyzer({
-  enabled: process.env.ANALYZE === "true",
-});
 
 const nextConfig: NextConfig = {
-  // your config
+  /* config options here */
 };
 
-export default pwaConfig(bundleAnalyzer(nextConfig));
+export default nextConfig;
